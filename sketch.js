80c27157a1c88b88d7be65543e6b68b724d1aed6
Kanish-Vishal <kanishvishal@gmail.com>
2025-05-09 20:41:49 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,7 @@
+function preload() {
+  font = loadFont("PoiretOne-Regular.ttf");
+}
+
 function setup() {
   createCanvas(boardSize, boardSize);
   cornerSize = boardSize / 8;
@@ -6,53 +10,66 @@ function setup() {
 
   textAlign(CENTER, CENTER);
   textSize(11);
-  //noLoop();
   textFont(font);
 
-  let rollBtn = createButton("Roll Dice");
+  rollBtn = createButton("Roll Dice");
   rollBtn.position(20, boardSize + 5);
-  rollBtn.mousePressed(rollDice);
-  
-  let propertyBuildings = {}; // key: property label, value: {residential: 0, industrial: 0}
-
+  rollBtn.mousePressed(() => {
+    rollDice();
+  });
 
   buyBtn = createButton("Buy Property");
   buyBtn.position(120, boardSize + 5);
-  buyBtn.mousePressed(buyProperty);
+  buyBtn.mousePressed(() => {
+    buyProperty();
+    buyBtn.hide();
+  });
   buyBtn.hide(); // Hide initially
 
   auctionBtn = createButton("Auction Property");
   auctionBtn.position(220, boardSize + 5);
-  auctionBtn.mousePressed(auctionProperty);
+  auctionBtn.mousePressed(() => {
+    auctionProperty();
+    auctionBtn.hide();
+  });
   auctionBtn.hide(); // Initially hidden
 
   bonusBtn = createButton("Add Bonus");
   bonusBtn.position(120, boardSize + 5);
-  bonusBtn.mousePressed(addBonus);
+  bonusBtn.mousePressed(() => {
+    addBonus();
+    bonusBtn.hide();
+  });
   bonusBtn.hide();
 
   hazardBtn = createButton("Add Hazard");
   hazardBtn.position(220, boardSize + 5);
-  hazardBtn.mousePressed(addHazard);
+  hazardBtn.mousePressed(() => {
+    addHazard();
+    hazardBtn.hide();
+  });
   hazardBtn.hide();
 
   buildBtn = createButton("Build");
-  buildBtn.position(10, boardSize + 100);
-  buildBtn.mousePressed(buildMenu);
-  buildBtn.hide();
-  
-  for (let label of propertyLabels) {
-  propertyBuildings[label] = { residential: 0, industrial: 0 };
-}
+  buildBtn.position(350, boardSize + 5);
+  buildBtn.mousePressed(() => {
+    buildMenu();
+    buildBtn.hide();
+  });
 
+  for (let label of propertyLabels) {
+    propertyBuildings[label] = { residential: 0, industrial: 0 };
+  }
 }
 
 function draw() {
   background(255);
-  strokeWeight(1.2);
+  strokeWeight(1);
+  textSize(12);
+
   drawBoard();
   drawPropertyTiles();
   drawChanceCard();
   drawTokens();
-  drawPlayerInfo();
+  industryTax();
 }
