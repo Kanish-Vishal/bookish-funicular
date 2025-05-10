817fe13c1357b47fbbfc08f1c54c2368edd6631d
Kanish-Vishal <kanishvishal@gmail.com>
2025-05-10 21:34:27 +0800

    Update ai.js

--- ai.js
+++ ai.js
@@ -27,21 +27,38 @@ function aiTurn() {
       }
     }
 
-    // If on Planning Permission, place bonus on own and hazard on opponent
+    // Lottery
     const label = labels[ai.pos];
-    if (label === "Planning\nPermission") {
-      const ownProps = Object.keys(propertyOwners).filter(
-        (p) => propertyOwners[p] === currentPlayer
-      );
-      if (ownProps.length) {
-        buildBonus(random(ownProps));
-      }
-      const oppProps = Object.keys(propertyOwners).filter(
-        (p) => propertyOwners[p] !== currentPlayer
-      );
-      if (oppProps.length) {
-        buildHazard(random(oppProps));
+    if (label === "Lottery") {
+      let lotteryMoney = floor(random(-3, 8));
+      player.money += lotteryMoney;
+      if (lotteryMoney >= 0) {
+        console.log(`${player.name} won $${lotteryMoney}M at a lottery.`);
+      } else {
+        let updatedLotteryMoney = lotteryMoney * -1;
+        console.log(
+          `${player.name} lost $${updatedLotteryMoney} at a lottery.`
+        );
       }
     }
   }
+
+  let aiPlayer = players[currentPlayer];
+  let totalIndustrialBuildings = 0;
+
+  for (const property in propertyOwners) {
+    if (propertyOwners[property] === currentPlayer) {
+      totalIndustrialBuildings += propertyBuildings[property]?.industrial || 0;
+    }
+  }
+
+  if (totalIndustrialBuildings > 0) {
+    console.log(
+      `${aiPlayer.name} has ${totalIndustrialBuildings} industrial buildings and pays $2.5M tax.`
+    );
+    aiPlayer.money -= 2.5;
+  } else {
+    console.log(`${player.name} has no industrial buildings. No tax paid.`);
+  }
 }
+
